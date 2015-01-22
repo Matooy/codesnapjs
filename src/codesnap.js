da32c1bfb361d600a5b818b3e904bef6169e69a5
松井 佑樹 <itunomanica@gmail.com>
2015-01-22 19:07:05 +0900

    Fix: remove duplicated hyphen in classname

--- src/codesnap.js
+++ src/codesnap.js
@@ -47,7 +47,7 @@ CodeSnap = function(O){
       description.innerHTML = file.description;
     var pre = document.createElement('pre');
       pre.setAttribute('id', C.prefix + file.name + '-code');
-      pre.setAttribute('class', C.prefix + '-code');
+      pre.setAttribute('class', C.prefix + 'code');
     el.appendChild(title);
     el.appendChild(description);
     el.appendChild(pre);
